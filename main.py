cbc870591997688b591404b1110cabd4bbb82f9f
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2025-01-05 23:12:59 +0200

    1.0

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from copy import deepcopy
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    users = deepcopy(users)
+    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    result = {}
+
+    for i in range (7):
+        for person in users:
+            cur_date = date.today() + timedelta(days=i)
+
+            if (person['birthday'].month, person['birthday'].day) == (cur_date.month, cur_date.day):
+                if cur_date.weekday() == 5 or cur_date.weekday() == 6:
+                    result['Monday'] = result.get('Monday', []) + [person['name']]
+                else:
+                    result[days_of_week[cur_date.weekday()]] = result.get(days_of_week[cur_date.weekday()], []) + [person['name']]
+
+    return result
 
 
 if __name__ == "__main__":
